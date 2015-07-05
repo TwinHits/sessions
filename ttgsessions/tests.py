d1b71cebc714c211916c11dbf4a5a721014fde91
twinhits <twinhits@gmail.com>
2015-07-05 19:15:21 -0400

    Fixed URL structures to accomodate sessions and characters inside a campaign
    Wrote tests for character pages

--- ttgsessions/tests.py
+++ ttgsessions/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.utils import timezone
 
-from .models import Campaign, Session, Note
+from .models import Campaign, Session, Note, Character
 
 import random
 
@@ -14,34 +14,38 @@ class SessionsViewsTest(TestCase):
         """create dummy data"""
         c = Campaign.objects.create(name="TestName", start_date=timezone.now())
         s = Session.objects.create(sess_date=timezone.now(), campaign=c)
+        char = Character.objects.create(name="TwinHits", campaign=c)
         cn = Note(content_object=c, note_text="campaign note", pub_date=timezone.now())
         cn.save()
         sn = Note(content_object=s, note_text="session note", pub_date=timezone.now())
         sn.save()
+        chn = Note(content_object=char, note_text="character note", pub_date=timezone.now())
+        chn.save()
 
 
     def test_home(self):
         """Test the home page, comparing list of all campaigns vs campaigns provided to view"""
         resp = self.client.get("/sessions/")
-        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.status_code, 200) 
         self.assertTrue("campaigns" in resp.context)
         self.assertListEqual(list(Campaign.objects.all()), list(resp.context["campaigns"]))
 
 
     def test_campaign_detail(self):
         """Test the campaign view with a random campaign"""
-        campaigns = Campaign.objects.all() 
-        c = random.choice(campaigns)
+        c = random.choice(Campaign.objects.all())
         #test 200 and data transfer
         resp = self.client.get("/sessions/" + str(c.pk) + "/")
         self.assertEqual(resp.status_code, 200)
         self.assertTrue("campaign" in resp.context)
         self.assertTrue("sessions" in resp.context)
         self.assertTrue("notes" in resp.context)
+        self.assertTrue("characters" in resp.context)
         #test campaign pk's on all data
         self.assertEqual(c.pk, resp.context["campaign"].pk)
         [self.assertTrue(s.campaign, c.pk) for s in resp.context["sessions"]]
-        [self.assertTrue(n.object_id, c.pk) for n in resp.context["notes"]]
+        [self.assertTrue(n.object_id, c.pk) for n in resp.context["notes"]] 
+        [self.assertTrue(ch.campaign, c.pk) for ch in resp.context["characters"]]
         #test 404
         resp = self.client.get("sessions/" + str(len(campaigns) + 1) + "/")
         self.assertEqual(resp.status_code, 404)
@@ -51,7 +55,7 @@ class SessionsViewsTest(TestCase):
         """Test the session detail page wth a random session."""
         s = random.choice(Session.objects.all())
         #test 200 and data transfer
-        resp = self.client.get("/sessions/" + str(s.campaign.pk) + "/" + str(s.pk) + "/")
+        resp = self.client.get("/sessions/" + str(s.campaign.pk) + "/session/" + str(s.pk) + "/")
         self.assertEqual(resp.status_code, 200)
         self.assertTrue("campaign" in resp.context)
         self.assertTrue("session" in resp.context)
@@ -60,3 +64,15 @@ class SessionsViewsTest(TestCase):
         self.assertTrue(s.pk, resp.context["session"].pk)
         self.assertEqual(s.campaign.pk, resp.context["campaign"].pk)
         [self.assertTrue(n.object_id, s.pk) for n in resp.context["notes"]]
+
+    def text_character_detail(self):
+        """Test the character detail page with a random character."""
+        ch = random.choice(Character.objects.all())
+        #test 200 and data transfer
+        resp = self.client.get("/sessions/" + str(ch.campaign.pk) + "/character/" + str(ch.pk) + "/")
+        resp.assertEqual(resp.status_code, 200)
+        resp.assertTrue("character" in resp.context)
+        resp.assertTrue("notes" in resp.context)
+        #test character pk's on all data
+        self.assertTrue(ch.pk, resp.context["character"].pk)
+        [self.assertTrue(n.object_id, ch.pk) for n in resp.context["notes"]]
--- ttgsessions/urls.py
+++ ttgsessions/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
         url(r"^$", views.sessions_home, name="home"),
         url(r"^(?P<c>[0-9]+)/$", views.campaign_detail, name="campaign_detail"),
         url(r"^new/$", views.campaign_new, name="campaign_new"),
-        url(r"^(?P<c>[0-9]+)/(?P<s>[0-9]+)/$", views.session_detail, name="session_detail"),
+        url(r"^(?P<c>[0-9]+)/session/(?P<s>[0-9]+)/$", views.session_detail, name="session_detail"),
     ]
